7085de38470878f80fdc9c64696da4b2a37c5777
Z510 <Z510@LENOVO>
2017-01-31 23:46:03 +0100

    Dependencies for Angular 2 and associated things

--- OnlineShop/Controllers/ValuesController.cs
+++ OnlineShop/Controllers/ValuesController.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Collections.Generic;
 using System.Web.Http;
 
 namespace OnlineShop.Controllers
 {
     [Authorize]
+    [AllowAnonymous]
     public class ValuesController : ApiController
     {
         // GET api/values
